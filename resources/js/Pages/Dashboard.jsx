ab7e8353dd2ac42b7d9b8970c1b5067947818f90
BayuAlif <bayualif170505@gmail.com>
2024-03-11 14:51:14 +0700

    pembuatan-sidebar

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,21 +1,58 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head } from "@inertiajs/react";
+import "tailwindcss/tailwind.css";
+import { IoIosSettings } from "react-icons/io";
 
-export default function Dashboard({ auth }) {
+import Sidebar from "./Partials/Sidebar";
+
+const Dashboard = () => {
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+        <div>
+            <div className="flex">
+                <div>
+                    <Sidebar />
+                </div>
+                <div className="p-5 flex justify-center space-x-4">
+                    <div className="flex space-x-4">
+                        <h1>Music</h1>
+                        <h1>Explore Premium</h1>
+                    </div>
+                    <div className="">
+                        <label className="input input-bordered flex items-center gap-2">
+                            <input
+                                type="text"
+                                className="grow"
+                                placeholder="Search"
+                            />
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 16 16"
+                                fill="currentColor"
+                                className="w-4 h-4 opacity-70"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </label>
+                    </div>
+                    <div className="flex space-x-4">
+                        <div>
+                            <IoIosSettings />
+                        </div>
+                        <div>
+                        <div className="avatar online">
+                            <div className="w-8 rounded-full">
+                                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            </div>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </AuthenticatedLayout>
+        </div>
     );
-}
+};
+
+export default Dashboard;
